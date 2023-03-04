de52b0c1cc8e7e76f43b6a2ff51f440633ed7d11
sonleminh <lms.smile2000@gmail.com>
2023-03-04 10:24:36 +0700

    fix env

--- index.js
+++ index.js
@@ -32,6 +32,49 @@ connection.once('open', () => {
   console.log('Connection successfully');
 });
 
+const io = require('socket.io')(9000, {
+  cors: {
+    origin: 'http://localhost:3000',
+  },
+});
+let users = [];
+
+const addUser = (userId, socketId) => {
+  !users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId });
+};
+
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userId) => {
+  return users.find((user) => user.userId === userId);
+};
+
+io.on('connection', (socket) => {
+  console.log('a user connected.');
+  // io.emit('Welcome', 'Hi this is socket server');
+  socket.on('addUser', (userId) => {
+    addUser(userId, socket.id);
+    io.emit('getUsers', users);
+  });
+
+  socket.on('sendMessage', ({ senderId, receiverId, text }) => {
+    const user = getUser(senderId);
+    io.to(user.socketId).emit('getMessage', {
+      senderId,
+      text,
+    });
+  });
+
+  socket.on('disconnect', () => {
+    console.log('a user disconnected!');
+    removeUser(socket.id);
+    io.emit('getUsers', users);
+  });
+});
+
 app.use('/api', authRouter);
 app.use('/api/user', userRouter);
 app.use('/api/post', postRouter);

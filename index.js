505b7e4326723474d3f2ed576e1712808d7cb959
sonleminh <lms.smile2000@gmail.com>
2023-03-19 01:13:40 +0700

    update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const session = require('express-session');
+const session = require('cookie-session');
 const cors = require('cors');
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
